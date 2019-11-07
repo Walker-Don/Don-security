0831f19f68abc73aa174324c5b206d26abfd129e
TJR_S <tjr.seeker@gmail.com>
2019-11-07 16:05:08 +0800

    5-8 绑定social账号

---
+++ imooc-security-core/src/main/java/com/imooc/security/core/social/ImoocConnectView.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package com.imooc.security.core.social;
+
+import org.springframework.web.servlet.view.AbstractView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * 因为是从微信扫码的页面转过来的，因此用json不好，最好给客户一个页面
+ */
+public class ImoocConnectView extends AbstractView {
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+	                                       HttpServletResponse response) throws Exception {
+
+		response.setContentType("text/html;charset=UTF-8");
+		if (model.get("connections") == null) {
+			response.getWriter().write("<h3>解绑成功</h3>");
+		} else {
+			response.getWriter().write("<h3>绑定成功</h3>");
+		}
+
+	}
+
+}
