23d31720a1a12d67ecf87311f0700f449eabf378
TJR_S <tjr.seeker@gmail.com>
2019-11-13 19:55:06 +0800

    6-4 重构用户密码登陆，在AuthenticationSuccessHandler中继续申请返回token

--- imooc-security-app/src/main/java/com/imooc/security/app/ImoocResourceServerConfig.java
+++ imooc-security-app/src/main/java/com/imooc/security/app/ImoocResourceServerConfig.java
@@ -1,17 +1,99 @@
-/**
- * 
- */
 package com.imooc.security.app;
 
+import com.imooc.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.imooc.security.core.properties.SecurityConstants;
+import com.imooc.security.core.properties.SecurityProperties;
+import com.imooc.security.core.validate.code.config.ValidateCodeSecurityConfig;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * @author zhailiang
- *
  */
 @Configuration
-@EnableResourceServer //资源服务器
-public class ImoocResourceServerConfig {
+@EnableResourceServer //资源服务器 focus 继承的类和browser是不一样的
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
+	@Autowired
+	private SecurityProperties securityProperties;
+
+	//@Autowired
+	//private UserDetailsService userDetailsService;
+	//
+	//@Autowired
+	//@Qualifier("dataSource")
+	//private DataSource dataSource;
+	//
+	@Autowired
+	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+	@Autowired
+	private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+	//@Autowired
+	//private SpringSocialConfigurer imoocSocialSecurityConfig;
+
+	@Autowired
+	private AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+
+	@Autowired
+	private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+	//需要预先认证的页面会调用security配置
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		logger.warn("启动HttpSecurity配置");
+
+		//封装passedUrls focus 这些url不能为null或者空
+		String[] urlsInternal = {
+				SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+				SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+				securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+				securityProperties.getBrowser().getLoginPage(),//登陆页面
+				securityProperties.getBrowser().getSignUpUrl(),//注册页面
+				securityProperties.getBrowser().getLogoutSuccessPage(),//退出登陆成功页面
+				SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*"};
+		String[] urlsExternal = StringUtils.splitByWholeSeparatorPreserveAllTokens(securityProperties.getBrowser().getPassedUrls(), ",");
+
+		String[] passedUrls = new String[urlsInternal.length + urlsExternal.length];
+		System.arraycopy(urlsInternal, 0, passedUrls, 0, urlsInternal.length);
+		System.arraycopy(urlsExternal, 0, passedUrls, urlsInternal.length, urlsExternal.length);
+
+		applyPasswordAuthenticationConfig(http);
+
+		//super.configure(http);
+		http
+				.apply(validateCodeSecurityConfig)
+				.and()
+				.apply(smsCodeAuthenticationSecurityConfig)
+				//.and()
+				//.apply(imoocSocialSecurityConfig)
+
+				.and()
+				.authorizeRequests()//需要请求授权
+				.antMatchers(passedUrls)
+				.permitAll()//跳过，不需要登陆，能够通过springSecurity自定义的filter，但是不一定能够通过自己定义的filter
+				.anyRequest().authenticated()//任何请求都需要身份认证
+
+				.and()
+				.csrf().disable();//关闭csrf
+	}
+
+	private void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
+		http.formLogin()
+				.loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+				.loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+				.successHandler(imoocAuthenticationSuccessHandler)
+				.failureHandler(imoocAuthenticationFailureHandler);
+	}
 }
--- imooc-security-app/src/main/java/com/imooc/security/app/authentication/ImoocAuthenticationSuccessHandler.java
+++ imooc-security-app/src/main/java/com/imooc/security/app/authentication/ImoocAuthenticationSuccessHandler.java
@@ -4,12 +4,18 @@
 package com.imooc.security.app.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.imooc.security.core.properties.LoginResponseType;
-import com.imooc.security.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -19,9 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 登陆成功操作
- *
- * @author zhailiang
+ * 适用于令牌模式的successHandler，用于在验证成功后分发令牌（OAuth2AccessToken）
  */
 @Component("imoocAuthenticationSuccessHandler")
 public class ImoocAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -32,21 +36,95 @@ public class ImoocAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
 	private ObjectMapper objectMapper;
 
 	@Autowired
-	private SecurityProperties securityProperties;
+	private ClientDetailsService clientDetailsService;
 
+	/**
+	 * 生成OAuth2AccessToken的service接口，自动注入进来 ：实现类DefaultTokenServices
+	 */
+	@Autowired
+	private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+	/**
+	 * 作用 ：验证成功后生成 OAuth2AccessToken
+	 * <p>
+	 * 过程 ：
+	 * <p>
+	 * 1. 构建验证过的 OAuth2Request（代表合法client的请求）
+	 * <p>
+	 * 2. 使用OAuth2Request（代表合法client的请求）和传进来的Authentication（代表合法用户）组合成合法的OAuth2Authentication
+	 * <p>
+	 * 3. 以OAuth2Authentication为参数使用AuthorizationServerTokenServices生成OAuth2AccessToken
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 	                                    Authentication authentication) throws IOException, ServletException {
 
 		logger.warn("登录成功");
-		//x系统设置返回类型为json，不跳转，返回用户信息
-		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginResponseType())) {
-			response.setContentType("application/json;charset=UTF-8");
-			response.getWriter().write(objectMapper.writeValueAsString(authentication));
+		//1. 构建验证过的 OAuth2Request（代表合法client）
+			//1.1 生成一个clientDetails
+					//a. 拿到Authorization头里面的clientId和clientSecret，basic64格式{ Authorization: Basic aW1vb2M6aW1vb2NzZWNyZXQ= }
+		String header = request.getHeader("Authorization");
 
-		} else {
-			super.onAuthenticationSuccess(request, response, authentication);
+		if (header == null || !header.startsWith("Basic ")) {
+			throw new UnapprovedClientAuthenticationException("请求头中无client信息");
 		}
+					//b. 解码base64格式，获得clientId，clientSecret
+		String[] tokens = extractAndDecodeHeader(header, request);
+		assert tokens.length == 2;
+
+		String clientId = tokens[0];
+		String clientSecret = tokens[1];
+
+					//c. 根据clientId查找ClientDetails
+		ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+					//d. 检查clientDetails是否为空，或者密码是否对的上
+		if (clientDetails == null) {
+			throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在:" + clientId);
+		} else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+			throw new UnapprovedClientAuthenticationException("clientSecret不匹配:" + clientId);
+		}
+
+			//1.2 生成tokenRequest，第一个参数是用来生成userAuthentication的,其实不应该是空，应该是原参数放进去
+		TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "password");
+
+			//1.3 生成oAuth2Request
+		OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+		//2. 组合成合法的OAuth2Authentication
+		OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+		//3. 生成 OAuth2AccessToken
+		OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+		//4. 把 OAuth2AccessToken 给前端
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(objectMapper.writeValueAsString(token));
+
+	}
+
+	/**
+	 * 解码header信息，特定basic auth的authentication。获取里面的clientID：clientSecret
+	 */
+	private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+
+		byte[] base64Token = header.substring(6).getBytes("UTF-8");
+		byte[] decoded;
+		try {
+			decoded = Base64.decode(base64Token);
+		} catch (IllegalArgumentException e) {
+			throw new BadCredentialsException("Failed to decode basic authentication token");
+		}
+
+		String token = new String(decoded, "UTF-8");
+
+		int delim = token.indexOf(":");
+
+		if (delim == -1) {
+			throw new BadCredentialsException("Invalid basic authentication token");
+		}
+		return new String[]{token.substring(0, delim), token.substring(delim + 1)};
 	}
 
 }
